c7d0d29906b6ec4b82db1b0defb334ddcb6b16a2
Magno Efren <58579707+MagnoEfren@users.noreply.github.com>
2025-05-30 14:56:13 -0500

    Update __init__.py

--- views/__init__.py
+++ views/__init__.py
@@ -1,3 +1,4 @@
-
-
-__all__ = [  ]
+from .dashboard_view import DashboardView
+from .graph_view import GraphView
+from .login_view import LoginView
+__all__ = [ "DashboardView", "GraphView", "LoginView"]
