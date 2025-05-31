5b0883c3cc4752b1d56c1fe8da7b496f7389ba74
Magno Efren <58579707+MagnoEfren@users.noreply.github.com>
2025-05-31 00:48:13 -0500

    Add files via upload

--- views/motor_view.py
+++ views/motor_view.py
@@ -1,11 +1,9 @@
 import flet as ft
-from components.signal_generator import SignalGenerator
-
+ 
 class MotorView:
     def __init__(self, page):
         self.page = page
-        self.signal = SignalGenerator().generate()
-
+    
     def go_back(self, e):
         self.page.go("/dashboard")
 
@@ -19,7 +17,7 @@ class MotorView:
                     leading=ft.IconButton(icon=ft.icons.ARROW_BACK, on_click=self.go_back),
                 ),
                 ft.Container(
-                    #
+                    content=ft.Text("Vista de prueba funcionando."),
                     padding=20,
                     expand=True
                 )
