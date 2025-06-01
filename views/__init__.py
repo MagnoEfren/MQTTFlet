c81368007cd1a70fb14d7b0eba8d2e3784d2ecd1
Magno Efren <58579707+MagnoEfren@users.noreply.github.com>
2025-05-31 19:09:58 -0500

    Update __init__.py

--- views/__init__.py
+++ views/__init__.py
@@ -1,8 +1,8 @@
-# from .dashboard_view import DashboardView
-# from .graph_view import GraphView
-# from .login_view import LoginView
-# from .motor_view import MotorView
-# from .sensors_view import SensorsView
-# from .settings_view import SettingsView
+from .dashboard_view import DashboardView
+from .graph_view import GraphView
+from .login_view import LoginView
+from .motor_view import MotorView
+from .sensors_view import SensorsView
+from .settings_view import SettingsView
 
-# __all__ = [ "DashboardView", "GraphView", "LoginView", "MotorView", "SensorsView", "SettingsView"]
+__all__ = [ "DashboardView", "GraphView", "LoginView", "MotorView", "SensorsView", "SettingsView"]
